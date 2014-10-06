2fbd529c0e9425836b243c8b9f5c566c73d6fff4
Ryan Owens <RyanOwens@linux.com>
2014-10-05 20:52:08 -0500

    main.py uses nanpy

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 __author__ = 'ryanvade'
-import time, serial, sys, os, errno
+#Program to be run on the raspberry pi
+import serial, time, os, sys, pprint, errno
 
-tty = "/dev/ttyAMA0"
+#variables
+pp = pprint.PrettyPrinter(indent=4)
+tty = "/dev/ttyACM0"
 
-try:#lets try to open the getty
-    with open(tty) as file:
-        pass
-except IOError as e:#if unable to do so...
+#Is the nanpy module available?
+try:
+    import nanpy
+    from nanpy import (Arduino, OneWire, Lcd, SerialManager, ArduinoApi, Stepper, Servo)
+except ImportError as e:
     print(e)
-    sys.exit(1)#exit with error
+    sys.exit(1)
 
-#port will be the connection to the arduino
-port = serial.Serial(tty, baudrate=115200, timeout=3.0)
+connectionName = SerialManager(device=tty)
+uno = ArduinoApi(connection=connectionName)
 
+#turn led on pin 13 on for 4 seconds then turn it off
+uno.pinMode(13,uno.OUTPUT)
+uno.digitalWrite(13, uno.HIGH)
+time.sleep(4)
+uno.digitalWrite(13, uno.LOW)
