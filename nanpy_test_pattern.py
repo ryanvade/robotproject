039d6e3ec2970811eba08f5e10e3461bbcd29a28
Ryan Owens <RyanOwens@linux.com>
2014-10-11 16:11:34 -0500

    main.py uses nanpy

--- nanpy_test_pattern.py
+++ nanpy_test_pattern.py
@@ -97,7 +97,7 @@ while message != "END":
     message = input('Please enter a direction: ')
     if message == "END":
         stop()
-        break
+       
     if message == "left":
         left()
     else:
