0e8914c19ca88edaf1eee9e6995b797cbe45f907
Ryan Owens <ryanvader82@gmail.com>
2014-10-25 15:41:57 -0500

    edited nanpy_test_pattern.py to use curses for keybinding support.

--- nanpy_test_pattern.py
+++ nanpy_test_pattern.py
@@ -1,6 +1,6 @@
 __author__ = 'ryanvade'
 # Program to be run on the raspberry pi
-import serial, time, os, sys, pprint, errno, csv, curses
+import serial, time, os, sys, pprint, errno, csv, pygame
 
 # is this an Arm system (raspberry pi)
 if not os.uname()[4].startswith("arm"):
@@ -34,8 +34,9 @@ message = " "
 screen = curses.initscr()
 defaultspeed = 127
 currentspeed = defaultspeed
-decreasespeedvalue = 10
-increasespeedvalue = 10
+leftspeed = defaultspeed
+decreasespeedvalue = 5
+increasespeedvalue = 5
 motor1PWM = 5
 motor3PWM = 6
 motor2PWM = 9
@@ -97,6 +98,14 @@ def setspeed(speed):
         print("Bad speed value")
 
 
+def setleftspeed(speed):
+    if (speed >= 0) & (speed <= 255):
+        uno.analogWrite(motor1PWM, speed)
+        uno.analogWrite(motor2PWM, speed)
+    else:
+        print("Bad speed value")
+
+
 def smoothleft(speedleft, speedright):
     uno.analogWrite(motor1PWM, speedleft)
     uno.analogWrite(motor2PWM, speedleft)
@@ -112,35 +121,6 @@ def smoothright(speedleft, speedright):
     uno.analogWrite(motor4PWM, speedright)
     forward()
 
-# while message != "END":
-#     message = input('Please enter a direction: ')
-#     if message == "END":
-#         stop()
-#         setspeed(0)
-#     elif message == "left":
-#         left()
-#     elif message == "right":
-#         right()
-#     elif message == "forward":
-#         forward()
-#     elif message == "reverse":
-#         reverse()
-#     elif message == "smoothleft":
-#         givenleftspeed = int(input("Enter left speed: "))
-#         givenrightspeed = int(input("Enter right speed: "))
-#         smoothleft(givenleftspeed, givenrightspeed)
-#     elif message == "smoothright":
-#         givenleftspeed = int(input("Enter left speed: "))
-#         givenrightspeed = int(input("Enter right speed: "))
-#         smoothright(givenleftspeed, givenrightspeed)
-#     else:
-#         stop()
-#     if (message != "END") & (message != "smoothleft") & (message != "smoothright"):
-#         givenspeed = int(input('Please enter a speed: '))
-#         setspeed(givenspeed)
-#stop()
-
-
 stdscr = curses.initscr()
 curses.cbreak()
 stdscr.keypad(1)
@@ -153,26 +133,34 @@ while key != ord('q'):
     key = stdscr.getch()
     stdscr.addch(20,25,key)
     stdscr.refresh()
+    setspeed(currentspeed)
+
     if key == curses.KEY_UP:
         stdscr.addstr(2, 20, "Up")
+        leftspeed = leftspeed + 1
+        setleftspeed(leftspeed)
         forward()
+        stdscr.addstr(1, 30, leftspeed)
     elif key == curses.KEY_DOWN:
         stdscr.addstr(3, 20, "Down")
+        leftspeed = leftspeed -1
+        setleftspeed(leftspeed)
+        stdscr.addstr(1, 30, leftspeed)
         reverse()
-    elif key == curses.KEY_LEFT:
-        stdscr.addstr(4, 20, "LEFT")
-        left()
-    elif key == curses.KEY_RIGHT:
-        stdscr.addstr(5, 20, "RIGHT")
-        right()
-    elif key == curses.KEY_NPAGE:
-        stdscr.addstr(6 , 20, "Next Page")
-        currentspeed = currentspeed + increasespeedvalue
-        setspeed(currentspeed)
-    elif key == curses.KEY_PPAGE:
-        stdscr.addstr(7, 20, "PREVIOUS Page")
-        currentspeed = currentspeed - decreasespeedvalue
-        setspeed(currentspeed)
+    # elif key == curses.KEY_LEFT:
+    #     stdscr.addstr(4, 20, "LEFT")
+    #     left()
+    # elif key == curses.KEY_RIGHT:
+    #     stdscr.addstr(5, 20, "RIGHT")
+    #     right()
+    # elif key == curses.KEY_NPAGE:
+    #     stdscr.addstr(6 , 20, "Next Page")
+    #     currentspeed = currentspeed + increasespeedvalue
+    #     setspeed(currentspeed)
+    # elif key == curses.KEY_PPAGE:
+    #     stdscr.addstr(7, 20, "PREVIOUS Page")
+    #     currentspeed = currentspeed - decreasespeedvalue
+    #     setspeed(currentspeed)
     elif key == ord("s"):
         stdscr.addstr(8, 20, "s")
         stop()
