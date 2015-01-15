fc9e6158451b1d4f592ab7692b33ada279de4da2
Ryan Owens <ryanvader82@gmail.com>
2015-01-14 22:11:24 -0600

    GTK_tests

--- Arduino Code/EPIR_Serial/EPIR_Serial.ino
+++ Arduino Code/EPIR_Serial/EPIR_Serial.ino
@@ -1,16 +1,22 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial ss(6,7);  // create SoftwareSerial to talk to the EPIR
-String buffer; //buffer string to hold each digit
+SoftwareSerial epir(6,7);  // create SoftwareSerial to talk to the EPIR  (RX, TX)
+String buffer;//buffer string to hold each digit
+String result;
 
 void setup()
 {
   Serial.begin(9600);
-  ss.begin(9600);
+  epir.begin(9600);
+  pinMode(4,OUTPUT);
+  
+  
 }
- 
 void loop()
 {
+  digitalWrite(4,1);
+  
+  
         while (Serial.available() > 0)
         {
                 char c = Serial.read();  //gets one byte (one digit) from serial buffer
@@ -23,18 +29,18 @@ void loop()
                 Serial.println("+<" + buffer + ">");  //Print the assembled string. Should match what was sent
                 int n = buffer.toInt();  //convert buffer into an integer
  
-                ss.write(n); //write the angle to the sensor
+                epir.write(n); //write the angle to the sensor
                
                 Serial.print("Rcv: "); //print the ack/nack message or present value (depending on command)
-               
                 buffer=""; //clear string for future input
-                while (ss.available() > 0)
+                while (epir.available() > 0)
                 {
-                        char c = ss.read();  //gets one byte (one digit) from serial buffer
-                        buffer += c; //adds digit to string
+                        char c = epir.read();  //gets one byte (one digit) from serial buffer
+                        result += c; //adds digit to string
                         delay(2);  //give buffer time to fill. probably not needed
                 }
-                Serial.println(buffer);
-                buffer = "";
+                Serial.println(result);
+                result = "";
         }
 }
+
---
+++ videoStream.py
@@ -0,0 +1,12 @@
+__author__ = 'ryanvade'
+from subprocess import Popen
+from os import system
+
+web_directory = '/var/nginx/www/live.m3u8'
+ts_directory = 'live/%08d.ts'
+video_device = '/dev/video0'
+#command = ['ffmpeg', '-f', 'video4linux2', '-y', '-r', '4', '-i', '/dev/video0', '-vf', '-vframes ','20', '-vcodec', 'mpeg4', 'out.mp4']
+command = ['ffmpeg', '-i live.h264', '-f s16le', '-i ', video_device, '-r:a 48000',  '-ac 2', '-c:v copy', '-c:a aac',
+           '-b:a 128k', '-map 0:0', '-map 1:0', '-f segment', '-segment_list ', web_directory,'-segment_time 8',
+           '-segment_format mpegts', '-segment_list_size 80', '-segment_list_flags live', ' -segment_wrap 10',
+           '-segment_list_type m3u8', '-strict', '-2', ts_directory, '< /dev/null']
