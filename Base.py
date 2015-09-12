8fb2938f228e4d09784759c2ab924436f7061c6d
Ryan Owens <RyanOwens@linux.com>
2015-09-12 14:17:43 -0500

    curses print

--- Base.py
+++ Base.py
@@ -3,11 +3,11 @@ import sys
 import BaseSerial
 
 # Is the RPi module available?
-try:
-    import RPi.GPIO as GPIO
-except ImportError as e:
-    print(e)
-    sys.exit(1)
+# try:
+#     import RPi.GPIO as GPIO
+# except ImportError as e:
+#     print(e)
+#     sys.exit(1)
 
 class Base:
     maxspeed = 255
@@ -18,6 +18,7 @@ class Base:
     decreasespeedvalue = 5
     increasespeedvalue = 5
     serialManager = None
+    rcar = '\r'.encode()
 
 
     def __init__(self, motor1PWM, motor2PWM, motor3PWM, motor4PWM, dir1, dir2, dir3, dir4):
@@ -29,34 +30,34 @@ class Base:
         self.dir2 = dir2
         self.dir3 = dir3
         self.dir4 = dir4
-        self.serialManager = BaseSerial.serialManager()
+        self.serialManager = BaseSerial.serialManager
 
     def stop(self):
-        self.serialManager.send_command('h', '\r')
+        self.serialManager.send_command('h', self.rcar)
         stop_ack = self.serialManager.receive_acknowledge()
         return stop_ack
 
 
 
     def forward(self, speed):
-        self.serialManager.send_command('df', speed, '\r')
+        self.serialManager.send_command('df', speed, self.rcar)
         drive_ack = self.serialManager.receive_acknowledge()
         return drive_ack
 
 
     def left(self, speed):
-        self.serialManager.send_command('tl', speed, '\r')
+        self.serialManager.send_command('tl', speed, self.rcar)
         left_ack = self.serialManager.receive_acknowledge()
         return left_ack
 
     def right(self, speed):
-        self.serialManager.send_command('tr', speed, '\r')
+        self.serialManager.send_command('tr', speed, self.rcar)
         right_ack = self.serialManager.receive_acknowledge()
         return right_ack
 
 
     def reverse(self, speed):
-        self.serialManager.send_command('db', speed, '\r')
+        self.serialManager.send_command('db', speed, self.rcar)
         reverse_ack = self.serialManager.receive_acknowledge()
         return reverse_ack
 
--- BaseSerial.py
+++ BaseSerial.py
@@ -3,19 +3,13 @@ __author__ = 'ryanvade'
 import sys
 import serial
 
-try:
-    import RPi.GPIO as GPIO
-except ImportError as e:
-    print(e)
-    sys.exit(1)
+# try:
+#     import RPi.GPIO as GPIO
+# except ImportError as e:
+#     print(e)
+#     sys.exit(1)
 
-class serialManager:
-    global expecting_response
-    global expecting_acknowledge
-    global character_code_list
-    global paramater_list
-    global response_list
-    global connection
+class BaseSerial:
     constant_communication = False
 
     def __init__(self, port="/dev/ttyACM0", baud_rate=19200):
@@ -25,36 +19,34 @@ class serialManager:
         self.expecting_acknowledge = False
 
         try:
-            connection = serial.Serial(port, baud_rate, timeout=1)
+            self.connection = serial.Serial(port, baud_rate, timeout=1)
         except serial.SerialException as e:
             print(e)
-            return None
+            exit(1)
 
         self.character_code_list = []
         self.paramater_list = []
         self.response_list = []
 
-        if(not connection.isOpen()):
-            print("No serial connection")
-            #TODO what to do if no connection is created
+        if(not self.connection.isOpen()):
+            print("No serial self.connection")
+            #TODO what to do if no self.connection is created
             sys.exit(1)
         else:
             print("Connection Established on:" +  port)
 
-        return self
-
     def send_command(self, character_code, paramater1 = None, paramater2 = None):
 
         try:
-            connection.write(character_code)
-            self.character_code_list.append(str(character_code))
+            self.connection.write(character_code.encode())
+            #self.character_code_list.append(str(character_code))
         except serial.SerialTimeoutException as e:
             print(e)
             return 1
 
         if(not paramater1 == None):
             try:
-                connection.write(paramater1)
+                self.connection.write(paramater1.encode())
                 self.paramater_list.append(str(paramater1))
             except serial.SerialTimeoutException as e:
                 print(e)
@@ -62,7 +54,7 @@ class serialManager:
 
         if(not paramater2 == None):
             try:
-                connection.write(paramater2)
+                self.connection.write(paramater2.encode())
                 self.paramater_list.append(str(paramater2))
             except serial.SerialTimeoutException as e:
                 print(e)
@@ -73,15 +65,15 @@ class serialManager:
     def receive_full_buffer(self):
         try:
             self.expecting_response = False
-            return connection.read(connection.inWaiting()) #return the full buffer
+            return self.connection.read(self.connection.inWaiting()) #return the full buffer
         except serial.SerialException as e:
             print(e)
-            return 0
+            exit(1)
 
     def receive_byte_buffer(self,size=1):#where size is the number of bytes
         try:
             self.expecting_response = False
-            return connection.read(size)
+            return self.connection.read(size)
         except serial.SerialException as e:
             print(e)
             return 0
@@ -89,7 +81,7 @@ class serialManager:
     def recive_full_line_buffer(self, size=None, eol='\n'): #where size is the max number of bytes
         try:
             self.expecting_response = False
-            return connection.readline(size, eol)
+            return self.connection.readline(size)
         except serial.SerialException as e:
             print(e)
             return 0
@@ -98,15 +90,16 @@ class serialManager:
         #TODO process for constant listening
         self.constant_communication = True
 
-    def get_response(self, type="Byte", size=1, eol='\n'):
+    def get_response(self, type="full", size=1):
         if type == "full":
             response = self.receive_full_buffer()
         elif type == "line":
-            response = self.recive_full_line_buffer(size, eol)
+            response = self.recive_full_line_buffer(size)
         else:
             response = self.receive_byte_buffer(size)
         self.response_list.append(response)
         self.expecting_response = False
+        print(response)
         return response
 
     def receive_acknowledge(self):
@@ -125,4 +118,4 @@ class serialManager:
         return self.paramater_list[-1]
 
     def close_connection(self):
-        connection.close()
+        self.connection.close()
